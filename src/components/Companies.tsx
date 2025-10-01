c7e41c22b84844ab00826bd77aa8f719758a2091
mizaan-sk <mizaandev@gmail.com>
2025-10-01 14:59:10 +0530

    Logos Updated

--- src/components/Companies.tsx
+++ src/components/Companies.tsx
@@ -5,14 +5,30 @@ import { motion } from "framer-motion";
 
 const Companies: React.FC = () => {
   const logos = [
-    "/logos/cprelogo.png",
-    "/logos/grayquestlogo.png",
-    "/logos/fms_logo.webp",
-    "/logos/Maplelogo.webp",
-    "/logos/makoons_logo.png",
-    "/logos/guardianonelogo.png",
-    "/logos/moh_logo.png",
-    "/logos/radcliffe_logo.png",
+    {src:"/logos/cprelogo.png",
+      pr:'0'
+    },
+    {src:"/logos/grayquestlogo.png",
+      pr:'0'
+    },
+    {src:"/logos/fms_logo.webp",
+      pr:'0'
+    },
+    {src:"/logos/Maplelogo.webp",
+      pr:'0'
+    },
+    {src:"/logos/makoons_logo.png",
+      pr:'20'
+    },
+    {src:"/logos/guardianonelogo.png",
+      pr:'0'
+    },
+    {src:"/logos/moh_logo.png",
+      pr:'0'
+    },
+    {src:"/logos/radcliffe_logo.png",
+      pr:'10'
+    },
   ];
 
   // Double logos (infinite seamless effect needs at least 2 sets)
@@ -27,7 +43,7 @@ const Companies: React.FC = () => {
 
         {/* Motion wrapper */}
         <motion.div
-          className="flex w-max gap-12 sm:gap-16 md:gap-20 lg:gap-24"
+          className="flex w-max gap-12 sm:gap-16 md:gap-20 lg:gap-35"
           animate={{ x: ["0%", "-50%"] }}
           transition={{
             duration: 15, // speed (lower = faster, e.g. 10)
@@ -41,11 +57,12 @@ const Companies: React.FC = () => {
               className="flex-shrink-0 flex items-center justify-center w-32 sm:w-40 md:w-48 lg:w-56 xl:w-60 h-20 sm:h-24 md:h-28 lg:h-32"
             >
               <Image
-                src={logo}
+                src={logo.src}
                 alt={`Company logo ${(index % logos.length) + 1}`}
                 width={240}
                 height={120}
                 className="w-full h-full object-contain grayscale hover:grayscale-0 transition-all duration-300 brightness-90 hover:brightness-100"
+                style={{ paddingRight: `${logo.pr}px` }}
                 priority={index < logos.length}
               />
             </div>
